a62cc5e3e02b15638d9ced9d3ca409cb1302966a
sragavan88 <sragavan88@users.noreply.github.com>
2015-04-09 16:20:46 +0530

    Update AIDS_in_America.f

--- AIDS_in_America.f
+++ AIDS_in_America.f
@@ -7,9 +7,11 @@
 
 	print *,'Enter the year desired'
 	read *,T
-
+	
+	!adding a character F with a length of 30, so that the end user is able to know which formula is being used to calculate.!
 	F='174.6*(T-1981.2)^3'
 	print *,"the formula being used is :",F
+	print *,
 
 	A=174.6*(T-1981.2)**3
 
