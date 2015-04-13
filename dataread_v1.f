3d69ab5ff8106e07b4a4da83f8605477ef4e4696
sragavan88 <sragavan88@users.noreply.github.com>
2015-04-13 13:55:22 +0530

    Update dataread_v1.f

--- dataread_v1.f
+++ dataread_v1.f
@@ -1,8 +1,9 @@
 	program dataread
-	! Simple program to read lines from a file and display the output. 
-	! Notes: Doesn't work if the data file has text characters. Separates each number based on whitespace, or a new line.
+	! Simple program to read 3 numerical values from a data input file and display the output. 
+	! Notes: Doesn't work if the data has text characters. Separates each number based on whitespace, or a new line.
+	! Requires the data file 'data.txt' to be saved in the appropriate location
 
-	open( 3,file= 'data.txt' )  !The location of data.txt or input file should be properly specified with the path, depending on compilation approach.
+	open( 3,file= 'data.txt' )  !The location of data.txt or input file should be properly specified with the path, depending on compilation approach. And the extension should be included, in windows.
 	read(3,*) A,B,C,D
 	print *, A,B
 	print *,C,D
